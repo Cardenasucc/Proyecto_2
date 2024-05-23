c42f88a8d93e40cebd8e9fc8ecba0265dd17dd1b
david inguilan <luis.inguilan@campusucc.edu.co>
2024-05-23 10:01:42 -0500

    actualizacion

--- cafeteria/settings.py
+++ cafeteria/settings.py
@@ -121,8 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'inventario/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- cafeteria/urls.py
+++ cafeteria/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
+    path('', include('inventario.urls')),
     path('admin/', admin.site.urls),
-    path('', include('inventario.urls'))
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
