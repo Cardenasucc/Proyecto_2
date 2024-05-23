ed8a1aebbd947a82d15c59bbb1a85dc0e608ebaa
david inguilan <luis.inguilan@campusucc.edu.co>
2024-05-23 10:20:21 -0500

    actualizacion

--- cafeteria/settings.py
+++ cafeteria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-52b$wzq7f876s!us@b_&a2&p5(nhpzud9+a*n3$#u@cy248@8d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'resta.azurewebsites.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'restaura.azurewebsites.net']
 
 
 
